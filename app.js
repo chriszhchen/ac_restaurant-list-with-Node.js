c227fc80eedb6066d59e87d618949982aca6c46d
Chris Chen <ok3201123@gmail.com>
2021-05-05 14:09:54 +0800

    feat: change data source of index, search and detail pages

--- app.js
+++ app.js
@@ -1,54 +1,73 @@
 // include express and define related variable
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurantData = require('./restaurant.json')
+const restaurantData = require('./restaurant.json') // 改為從資料庫拿
 const mongoose = require('mongoose')
-const Restaurant = require('../restaurant.js')
+const Restaurant = require('./models/restaurant.js')
 
 const app = express()
 const port = 3000
 const stylesheetPath = { index: '/stylesheets/index.css', show: '/stylesheets/show.css' }
 
+// modules setting
 mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
-
 
 db.on('error', () => {
   console.log('mongodb error!')
 })
-
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-
 app.set('view engine', 'handlebars')
-
-// setting static files
-
 app.use(express.static('public'))
 
-// setting the route and response
+// --- route setting ---
+
+// render index page
 app.get('/', (req, res) => {
-  // console.log(restaurantData.results)
-  res.render('index', { stylesheetPath: stylesheetPath.index, restaurants: restaurantData.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { stylesheetPath: stylesheetPath.index, restaurants }))
+    .catch(err => console.log(err))
 })
-
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  // console.log(req)
-  const chosenRestaurant = restaurantData.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { stylesheetPath: stylesheetPath.show, restaurant: chosenRestaurant })
-})
+// render search results
 app.get('/search', (req, res) => {
-  // console.log(req)
-  const filteredRestaurants = restaurantData.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(req.query.keyword.trim().toLowerCase())
-  })
-  res.render('index', { stylesheetPath: stylesheetPath.index, restaurants: filteredRestaurants, keyword: req.query.keyword })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      const filteredRestaurants = restaurants.filter(restaurant => {
+        return restaurant.name.toLowerCase().includes(req.query.keyword.trim().toLowerCase())
+      })
+      res.render('index', { stylesheetPath: stylesheetPath.index, restaurants: filteredRestaurants, keyword: req.query.keyword })
+    })
+    .catch(err => console.log(err))
 })
 
+// render create page
+
+// CREATE function
+
+// READ function and render detail page
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const id = req.params.restaurant_id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => {
+      res.render('detail', { stylesheetPath: stylesheetPath.show, restaurant })
+    })
+    .catch(err => console.log(err))
+})
+
+// render edit page
+
+
+// UPDATE function
+
+
+// DELETE function
 
 // start and listen the server
 app.listen(port, () => {
